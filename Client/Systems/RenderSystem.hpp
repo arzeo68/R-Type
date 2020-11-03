04c541bee7a1864413441d42fcfbc0a4714c7ea5
Quentin Tessier <quentin.tessier@epitech.eu>
2020-11-03 10:17:38 +0100

    Adding SFML specific component

---
+++ Client/Systems/RenderSystem.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+// #include <ECS/System.hpp>
+
+class RenderSystem /* : public System */ {
+    public:
+        void init();
+
+        void update(float delta /*, RenderTarget, World */);
+    private:
+};
