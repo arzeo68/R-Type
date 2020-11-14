0c42fca48abe7dc67976515d55d53c3ed17a1238
alexis <alexis.walter@epitech.eu>
2020-11-14 11:46:24 +0100

    feat(master) remove hexadecimal print, change window size, comment dome code

--- src/Client/EventManager/SFMLEvents.hpp
+++ src/Client/EventManager/SFMLEvents.hpp
@@ -5,19 +5,36 @@
 #include <Client/typeEncapsulation.hpp>
 #include <ObserverPattern/Subject.hpp>
 
+/**
+ * main namespace
+ */
 namespace Rtype
 {
-
+    /**
+     * event called when the user close the window
+     */
 class EventClose : public Observer::IEvent {
 public:
+    /**
+     * constructor
+     */
     EventClose()
     { }
-
+    /**
+     * destructor
+     */
     ~EventClose() = default;
 };
-
+    /**
+     * event called when the user resize the window
+     */
 class EventResized : public Observer::IEvent {
 public:
+    /**
+     *
+     * @param x new width
+     * @param y new heigth
+     */
     EventResized(unsigned int x, unsigned int y)
         : new_size(x, y)
     { }
@@ -26,9 +43,15 @@ public:
 
     vec2i new_size;
 };
-
+/**
+ * event called when the user is focusing the window
+ */
 class EventFocus : public Observer::IEvent {
 public:
+    /**
+     *
+     * @param hasFocus true if the user is focusing the window, otherwise false
+     */
     EventFocus(bool hasFocus)
         : hasFocus(hasFocus)
     { }
@@ -37,9 +60,15 @@ public:
 
     bool hasFocus;
 };
-
+    /**
+     * event called when the user press a key
+     */
 class EventKeyPressed : public Observer::IEvent {
     public:
+    /**
+     *
+     * @param key key that is pressed by the user
+     */
     EventKeyPressed(keys key)
         : _key(key)
     { }
@@ -49,8 +78,15 @@ class EventKeyPressed : public Observer::IEvent {
     keys _key;
 };
 
+/**
+ * event called when the user is typing text
+ */
 class EventText : public Observer::IEvent {
 public:
+    /**
+     *
+     * @param input that the user typed
+     */
     EventText(uint32_t text)
         : unicode(text)
     { }
@@ -59,9 +95,17 @@ public:
 
     uint32_t unicode;
 };
-
+    /**
+     * event called when the user scroll
+     */
 class EventMouseWheel : public Observer::IEvent {
 public:
+    /**
+     *
+     * @param delta scroll delta
+     * @param x mouse x coordinate
+     * @param y mouse y coordinate
+     */
     EventMouseWheel(float delta, unsigned int x, unsigned int y)
         : delta(delta), position(x, y)
     { }
@@ -71,21 +115,34 @@ public:
     float delta;
     vec2i position;
 };
-
+    /**
+     * event called when the user click on any mouse button
+     */
 class EventMouseButton : public Observer::IEvent {
 public:
+    /**
+     * state of the button mouse triggerd
+     */
     enum State {
         PRESSED,
         RELEASED
     };
-
+    /**
+     * button that trigered the event
+     */
     enum Button {
         LEFT,
         RIGHT,
         WHEEL,
         EXTRA
     };
-
+    /**
+     *
+     * @param state the state of the button mouse triggerd
+     * @param which button that trigered the event
+     * @param x click x coordinate
+     * @param y click y coordinate
+     */
     EventMouseButton(State state, sf::Mouse::Button which, unsigned int x, unsigned int y)
         : state(state), button(LEFT), position(x, y)
     {
@@ -111,9 +168,16 @@ public:
     Button button;
     vec2i position;
 };
-
+    /**
+     * event called when the user move his mouse
+     */
 class EventMouseMove : public Observer::IEvent {
 public:
+    /**
+     *
+     * @param x new x position
+     * @param y new y position
+     */
     EventMouseMove(unsigned int x, unsigned int y)
         : position(x, y)
     { }
